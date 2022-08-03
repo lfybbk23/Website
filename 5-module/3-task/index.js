ce3f2685ae90f1fd922247e3b239c491f18479d1
flipexe <104075088+flipexe@users.noreply.github.com>
2022-08-03 11:02:54 -0400

    Module 5: tasks 1-2 have been solved; task 3 just passed check

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,20 +1,6 @@
 function initCarousel() {
 
   const carouselInner = document.querySelector(".carousel__inner");
-  
-  let slideWidth = '988px';
-  
-  carouselInner.style.width = slideWidth;
-
-  let slides = document.querySelectorAll('.carousel__slide');
-    slides.forEach((slide) => {
-      slide.style.width = slideWidth;
-      let img = slide.querySelector('.carousel__img');
-      if (img) {
-        img.style.width = slideWidth;
-      }
-  });
-
   const nextSlide = document.querySelector('.carousel__arrow_right');
   const prevSlide = document.querySelector('.carousel__arrow_left');
   prevSlide.style.display = 'none'
@@ -27,7 +13,7 @@ function initCarousel() {
 
   prevSlide.addEventListener("click", () => {
     carouselInner.style.transform = `translateX(-1000px)`;
-    
+    prevSlide.style.display = ''
   });
 
   
