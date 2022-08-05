7e6d68bfbbf52171991af683dd98c370ef8db8e4
flipexe <104075088+flipexe@users.noreply.github.com>
2022-08-04 22:43:02 -0400

    Module 5: task 3 has been fixed.

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -4,14 +4,16 @@ function initCarousel() {
   const slideWidth = document.querySelector(".carousel__inner").offsetWidth;
   const nextSlide = document.querySelector('.carousel__arrow_right');
   const prevSlide = document.querySelector('.carousel__arrow_left');
-  const counter = 0;
+
+  let counter = 0;
 
   carouselArrowDisplay = () => {
     counter > 2 ? nextSlide.style.display = 'none' : nextSlide.style.display = '';
     counter > 0 ? prevSlide.style.display = '' : prevSlide.style.display = 'none';
   }
-  carouselArrowDisplay();
 
+  carouselArrowDisplay();
+  
   nextSlide.addEventListener("click", () => {
     counter++;
     carouselInner.style.transform = 'translateX(' + (-slideWidth * counter) + 'px)';
